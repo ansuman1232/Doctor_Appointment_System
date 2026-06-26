6805892483d247ca4e57d36631a7e85cd55e07d4
Ansuman Mishra <anshumanmishra1232@gmail.com>
2026-06-26 09:45:50 +0530

    bug fix

--- frontend/frontendReact/src/pages/MyAppointment.jsx
+++ frontend/frontendReact/src/pages/MyAppointment.jsx
@@ -1,7 +1,7 @@
 import { useEffect,useState,useContext } from "react";
 import axios from 'axios';
 import {AuthContext} from '../context/AuthContext.jsx'
-
+import NavMenu from "../utils/NavMenu.jsx";
  function MyAppointment(){
    const [appointments,setAppointments]=useState([]);
     const {userId} =useContext(AuthContext)
@@ -21,7 +21,10 @@ import {AuthContext} from '../context/AuthContext.jsx'
    },[userId])
 
     return (<>
+        <NavMenu/>
    {
+
+
    
   appointments.length>0 ? appointments.map(e=><div key={e._id} style={{color:"black",marginBottom:"3vw"}}>
     <p>DoctorId: {e.doctorId}</p>
